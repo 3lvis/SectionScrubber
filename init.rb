48e2d3b0452031fc833a8460c46ebdb895b47475
Christoffer Winterkvist <christoffer@winterkvist.com>
2015-03-11 19:59:41 +0100

    Refactor init.rb

--- init.rb
+++ init.rb
@@ -1,3 +1,5 @@
+require 'fileutils'
+
 print 'pod name > '
 cocoapod = gets.chop
 file_names = Dir["#{Dir.pwd}/**/*.*"]
@@ -10,7 +12,12 @@ ignored_file_types = ['.xccheckout',
 file_names.each do |file_name|
     if !ignored_file_types.include?(File.extname(file_name))
         text = File.read(file_name)
-        new_contents = text.gsub(/CocoaPod/, cocoapod)
+        new_contents = text.gsub(/Foobar/, cocoapod)
         File.open(file_name, "w") {|file| file.puts new_contents }
     end
 end
+
+git_directory = "#{Dir.pwd}/.git"
+FileUtils.rm_rf git_directory
+
+system("git init && git add . && git commit -am 'Initial commit'")
