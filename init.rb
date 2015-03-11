6ea12937e1c9036b95d5a3d7c445f8ae212c5d70
Christoffer Winterkvist <christoffer@winterkvist.com>
2015-03-11 20:54:18 +0100

    Rename pod spec and add description

--- init.rb
+++ init.rb
@@ -1,7 +1,12 @@
 require 'fileutils'
 
+folder_path = Dir.pwd
+
 print 'pod name > '
-cocoapod = gets.chop
+pod_name = gets.chop
+print 'pod description > '
+pod_description = gets.chop
+
 file_names = Dir["#{Dir.pwd}/**/*.*"]
 ignored_file_types = ['.xccheckout',
                       '.xcodeproj',
@@ -12,10 +17,13 @@ ignored_file_types = ['.xccheckout',
 file_names.each do |file_name|
     if !ignored_file_types.include?(File.extname(file_name))
         text = File.read(file_name)
-        new_contents = text.gsub(/Foobar/, cocoapod)
+        new_contents = text.gsub(/<PODNAME>/, pod_name)
+        new_contents = text.gsub(/<PODDESCRIPTION>/, pod_name)
         File.open(file_name, "w") {|file| file.puts new_contents }
     end
 end
+
+File.rename("#{Dir.pwd}/PODNAME.podspec", "#{Dir.pwd}/#{pod_name}.podspec")
 
 git_directory = "#{Dir.pwd}/.git"
 FileUtils.rm_rf git_directory
