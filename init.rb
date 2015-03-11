0973a6c455a550861a650daee583c60a78ba3f67
Christoffer Winterkvist <christoffer@winterkvist.com>
2015-03-11 21:15:06 +0100

    Update init.rb

--- init.rb
+++ init.rb
@@ -31,3 +31,4 @@ FileUtils.rm_rf git_directory
 
 system("git init && git add . && git commit -am 'Initial commit'")
 system("git remote add origin https://github.com/hyperoslo/#{pod_name}.git")
+FileUtils.rm('init.rb')
