e53b0c204058b4e7b7e986dfd238128082de8bce
Christoffer Winterkvist <christoffer@winterkvist.com>
2015-03-11 21:29:38 +0100

    Remove replacing description

--- init.rb
+++ init.rb
@@ -4,8 +4,6 @@ folder_path = Dir.pwd
 
 print 'pod name > '
 pod_name = gets.chop
-print 'pod description > '
-pod_description = gets.chop
 
 file_names = Dir["#{Dir.pwd}/**/*.*"]
 ignored_file_types = ['.xccheckout',
@@ -19,7 +17,6 @@ file_names.each do |file_name|
     if !ignored_file_types.include?(File.extname(file_name))
         text = File.read(file_name)
         new_contents = text.gsub(/<PODNAME>/, pod_name)
-        new_contents = text.gsub(/<PODDESCRIPTION>/, pod_description)
         File.open(file_name, "w") {|file| file.puts new_contents }
     end
 end
