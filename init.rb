506ef85e6c0982a5633a032ab76163a628bb17a0
Christoffer Winterkvist <christoffer@winterkvist.com>
2015-03-17 15:41:21 +0100

    Update init.rb

--- init.rb
+++ init.rb
@@ -4,8 +4,14 @@ require 'fileutils'
 
 folder_path = Dir.pwd
 
-print 'pod name > '
-pod_name = gets.chop
+if ARGV[0]
+  pod_name = ARGV[0]
+else
+  print 'pod name > '
+  pod_name = gets.chop
+end 
+
+
 
 author_name = 'Hyper Interaktiv AS'
 author_email = 'ios@hyper.no'
