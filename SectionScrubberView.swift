cdf40040e297ec01ec1c1b998b6ddd610d91590e
Igor Ranieri Elland <igor@elland.me>
2016-05-02 15:07:15 +0200

    Added skeleton scrubber view.

---
+++ SectionScrubberView.swift
@@ -0,0 +1,18 @@
+//
+//  SectionScrubberView.swift
+//  CollectionSectionScroller
+//
+//  Created by Igor Ranieri on 02.05.16.
+//
+//
+
+import UIKit
+
+/*
+ This will be the main client interface. Ideally, to use this, the user needs only initialise it with a data source, a delegate and add it to the view hierarchy.
+ It will have its own gesture recogniser, and based on the data sources list of section titles, display them accordingly as a side-label.
+ It will also notify its delegate of the section change, so the delegate can scroll the collection/tableview to the corresponding section.
+ */
+class SectionScrubberView: UIView {
+
+}
