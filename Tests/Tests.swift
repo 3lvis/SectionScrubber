121481bc2de63c3093e11e277414fd6a4a98fb49
marijnschilling <marijnschilling@gmail.com>
2016-06-02 17:41:45 +0200

    Add tests for the view to contentView translations

--- Tests/Tests.swift
+++ Tests/Tests.swift
@@ -1,9 +1,30 @@
 import UIKit
 import XCTest
 
+@testable import iOS
+
 class Tests: XCTestCase {
-    func test() {
-        let ofCourse = true
-        XCTAssertEqual(ofCourse, true)
+
+    func testCalculateYPosInView() {
+
+        let dateScrubber = DateScrubber()
+
+        dateScrubber.containingViewFrame = CGRectMake(0, 0, 0, 100)
+        dateScrubber.containingViewContentSize = CGSizeMake(0, 200)
+
+        let result = dateScrubber.calculateYPosInView(forYPosInContentView: 100)
+
+        XCTAssert(result == 50.0)
+    }
+
+    func testCalculateYPosInContentView() {
+        let dateScrubber = DateScrubber()
+
+        dateScrubber.containingViewFrame = CGRectMake(0, 0, 0, 100)
+        dateScrubber.containingViewContentSize = CGSizeMake(0, 200)
+
+        let result = dateScrubber.calculateYPosInContentView(forYPosInView: 50)
+
+        XCTAssert(result == 100.0)
     }
 }
