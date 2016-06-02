a036df0877ec1e234dd8eea85929ae9c3443019d
marijnschilling <marijnschillin@gmail.com>
2016-06-02 12:49:43 +0200

    Drag the yPos of the view with a gesture recognizer

--- Sources/DateScrubber.swift
+++ Sources/DateScrubber.swift
@@ -10,10 +10,20 @@ import UIKit
 
 public class DateScrubber: UIViewController {
 
+    let dragGestureRecognizer = UIPanGestureRecognizer()
+
+    /// the minimum position for the date scrubber from the top, can be overwritten by the client
+    public var minimumPosition : CGFloat = 64.0
+
     override public func viewDidLoad() {
         super.viewDidLoad()
 
         self.view.backgroundColor = UIColor.yellowColor()
+
+
+        self.dragGestureRecognizer.addTarget(self, action: #selector(handleDrag))
+        self.view.addGestureRecognizer(self.dragGestureRecognizer)
+
     }
 
     override public func viewWillLayoutSubviews() {
@@ -24,8 +34,18 @@ public class DateScrubber: UIViewController {
     public func updateFrame(scrollView scrollView: UIScrollView) {
 
         let scrollPercentage = scrollView.contentOffset.y / scrollView.contentSize.height
-        let yPos = scrollView.bounds.size.height * scrollPercentage
+        let yPos = (scrollView.bounds.size.height - minimumPosition) * scrollPercentage
 
-        view.frame = CGRectMake(0,yPos,44,44)
+        view.frame = CGRectMake(0,yPos + minimumPosition, 44,44)
+    }
+
+    func handleDrag(gestureRecognizer : UIPanGestureRecognizer) {
+        if gestureRecognizer.state == .Began || gestureRecognizer.state == .Changed {
+
+            let translation = gestureRecognizer.translationInView(self.view)
+            // note: 'view' is optional and need to be unwrapped
+            gestureRecognizer.view!.frame.origin = CGPointMake(0, gestureRecognizer.view!.frame.origin.y + translation.y)
+            gestureRecognizer.setTranslation(CGPointMake(0,0), inView: self.view)
+        }
     }
 }
