c8a73922b49049e53b5673da1d95bb593025211f
marijnschilling <marijnschilling@gmail.com>
2016-06-03 10:47:06 +0200

    Add tests for view with offset (they fail :( )

--- Tests/Tests.swift
+++ Tests/Tests.swift
@@ -5,7 +5,7 @@ import XCTest
 
 class Tests: XCTestCase {
 
-    func testCalculateYPosInView() {
+    func moveTheScrubberWhenScrollingTheCollectionView() {
 
         let dateScrubber = DateScrubber()
 
@@ -17,7 +17,7 @@ class Tests: XCTestCase {
         XCTAssert(result == 50.0)
     }
 
-    func testCalculateYPosInContentView() {
+    func scrollCollectionViewWhenMovingTheScrubber() {
         let dateScrubber = DateScrubber()
 
         dateScrubber.containingViewFrame = CGRectMake(0, 0, 0, 100)
@@ -26,5 +26,32 @@ class Tests: XCTestCase {
         let result = dateScrubber.calculateYPosInContentView(forYPosInView: 50)
 
         XCTAssert(result == 100.0)
+    }
+
+    func moveTheScrubberWhenScrollingTheCollectionViewWithOffset() {
+
+        let dateScrubber = DateScrubber()
+
+        let offset : CGFloat = 10.0
+
+        dateScrubber.containingViewFrame = CGRectMake(0, offset, 0, 100)
+        dateScrubber.containingViewContentSize = CGSizeMake(0, 200)
+
+        let result = dateScrubber.calculateYPosInView(forYPosInContentView: 0)
+
+        XCTAssert(result == offset)
+    }
+
+    func scrollCollectionViewWithOffsetWhenMovingTheScrubber() {
+        let dateScrubber = DateScrubber()
+
+        let offset : CGFloat  = 10.0
+
+        dateScrubber.containingViewFrame = CGRectMake(0, offset, 0, 100)
+        dateScrubber.containingViewContentSize = CGSizeMake(0, 200)
+
+        let result = dateScrubber.calculateYPosInContentView(forYPosInView: offset)
+
+        XCTAssert(result == 0)
     }
 }
