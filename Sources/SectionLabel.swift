d8d292afc886b7a306809d94d1ccb5a4f7f896da
marijnschilling <marijnschilling@gmail.com>
2016-06-06 17:28:36 +0200

    Add the interface of necessary functions

--- Sources/SectionLabel.swift
+++ Sources/SectionLabel.swift
@@ -12,6 +12,8 @@ class SectionLabel: UIView {
 
     let sectionLabelImageView = UIImageView()
 
+    let textLabel = UILabel()
+
     var labelImage: UIImage? {
         didSet {
 
@@ -22,4 +24,25 @@ class SectionLabel: UIView {
             }
         }
     }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        self.addSubview(textLabel)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+
+    override func layoutSubviews() {
+//          self.textLabel.frame = CGRectMake()
+    }
+
+    func setText(text: String){}
+
+    func hide(){}
+
+    func show(){}
 }
