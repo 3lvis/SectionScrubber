44e61e260e2b3f1176c0f7de353de20095b77753
Elvis Nuñez <elvisnunez@me.com>
2016-06-09 16:10:41 +0200

    Make UILabel extension internal

--- Sources/UILabel+Sweetness.swift
+++ Sources/UILabel+Sweetness.swift
@@ -1,8 +1,7 @@
 import UIKit
 
-public extension UILabel {
-  
-    public func width() -> CGFloat {
+extension UILabel {
+func width() -> CGFloat {
         let attributes = [NSFontAttributeName : self.font]
         let rect = (self.text ?? "" as NSString).boundingRectWithSize(CGSize(width: CGFloat.max, height: CGFloat.max), options: .UsesLineFragmentOrigin, attributes: attributes, context: nil)
         
